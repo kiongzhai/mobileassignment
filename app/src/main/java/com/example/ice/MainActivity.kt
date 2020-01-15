db166f03613ffe4b78ddf759270a5614c49b924c
Tang Her Ment <tanghm-wp17@student.tarc.edu.my>
2020-01-15 19:21:49 +0800

    login and registration got database

--- app/src/main/java/com/example/ice/MainActivity.kt
+++ app/src/main/java/com/example/ice/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.ice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_registration.*
 
@@ -12,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvSignUp.setOnClickListener(){
+
+
+        tvSignUp.setOnClickListener() {
             val intent = Intent(this, Registration::class.java)
             startActivity(intent)
 
@@ -20,11 +24,31 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        buttonLogin.setOnClickListener(){
+        buttonLogin.setOnClickListener() {
+
             val intent = Intent(this, Recommendation::class.java)
             startActivity(intent)
 
+            val email = editTextEmailLogin.text.toString()
+            val password = editTextPasswordReg.text.toString()
+
+
+
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+
+                        Toast.makeText(this, "Logging in!", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+                .addOnFailureListener{
+                    Toast.makeText(this, "Invalid Email/Password!", Toast.LENGTH_SHORT).show()
+                }
+
         }
+
 
     }
 }
--- app/src/main/java/com/example/ice/Registration.kt
+++ app/src/main/java/com/example/ice/Registration.kt
@@ -3,6 +3,9 @@ package com.example.ice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_registration.*
 
 class Registration : AppCompatActivity() {
@@ -12,9 +15,49 @@ class Registration : AppCompatActivity() {
         setContentView(R.layout.activity_registration)
 
         btnDoneSignUp.setOnClickListener(){
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            performRegister()
 
         }
     }
+
+    private fun performRegister(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+        val email = editTextEmailReg.text.toString()
+        val password = editTextPasswordReg.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter an Email/Password", Toast.LENGTH_SHORT).show()
+            return
+
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if (!it.isSuccessful)
+                    return@addOnCompleteListener
+
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Failed to create user", Toast.LENGTH_SHORT).show()
+
+                saveUserToFirebaseDatabase()
+            }
+    }
+    private fun saveUserToFirebaseDatabase() {
+
+
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/user/$uid")
+
+
+        val user = User(uid, editTextFirstName.text.toString(), editTextSurname.text.toString(), editTextUsernameReg.text.toString(), editTextPasswordReg.text.toString(), editTextEmailReg.text.toString(), editTextMobileNum.text.toString())
+        ref.setValue(user)
+    }
+
+
 }
+
+class User(val uid : String ,val firstname : String, val surname : String, val username : String, val password : String,  val email : String, val mobileno : String )
+
